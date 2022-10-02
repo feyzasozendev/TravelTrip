8c48373cd1e0becf1332a396a7d604419b7583a1
feyza ışık <feyza_43_4335@hotmail.com>
2022-10-03 00:44:39 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/RehberController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TravelTripProje.Models.Siniflar;
+using System.Web.Mvc;
+
+namespace TravelTripProje.Controllers
+{
+    public class RehberController : Controller
+    {
+        // GET: Rehber
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+
+    }
+}
---
+++ TravelTripProje/Controllers/İletisimController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelTripProje.Controllers
+{
+    public class İletisimController : Controller
+    {
+        // GET: Iletisim
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
